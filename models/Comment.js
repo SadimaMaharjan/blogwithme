fde6ecf5c63c06d413774e0e3911dfbbb5f2c29c
Sadima Maharjan <npsadimamaharjan@gmail.com>
2023-06-09 11:05:26 +1000

    added connection file and wrote User, Post and Comment models

---
+++ models/Comment.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  comment_text: {
+    type: DataTypes.STRING,
+  },
+  post_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "post",
+      key: "id",
+    },
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "user",
+      key: "id",
+    },
+  },
+});
